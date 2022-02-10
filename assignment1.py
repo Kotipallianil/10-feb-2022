271f00829f3fac0723a18754a57c44b4c6539cf6
Kotipallianil <99274818+Kotipallianil@users.noreply.github.com>
2022-02-10 21:47:45 +0530

    assignment1.y

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+sno=int(input('enter the student no:'))
+sname=int(input('enter the student name:'))
+sgruop=int(input('enter the student gruop:'))
+s1=int(input('enter the physics marks:'))
+s2=int(input('enter the chemistry marks:'))
+s3=int(input('enter the operating system marks:'))
+s4=int(input('enter the telugu marks:'))
+s5=int(input('enter the java marks:'))
+s6=int(input('enter the hindi  marks:'))
+if s1>=30 and s2>=30 and s3>=30 and s4>=30 and s5>=30 and s6>=30:
+ print('pass')
+else:
+    print('fail')
